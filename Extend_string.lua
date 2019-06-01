fc93dc04fc81ded6494d771521898ab084837685
Henrique santos de Lima <hslhenriquegoscara@gmail.com>
2019-06-01 12:17:28 -0300

    add a new function

--- Extend_string.lua
+++ Extend_string.lua
@@ -1,1 +1,17 @@
-string.toArray = function (s)  local result ={} for i=0,s:len(),1 do result[i]=s:sub(i,i) end return result end
+--[ transformar uma string em um array de char]--
+string.toArray = function (s)  
+                    local result ={} 
+                    for i=0,s:len(),1 do 
+                       result[i]=s:sub(i,i) 
+                    end 
+                    return result 
+                  end
+--[ transformar um array de char em uma string]--
+string.arrayToString = function (array,fim) 
+                            local result =""    
+                            for i =1,#array,1 do
+                               if i>fim then break end
+                                result= result.. array[i] 
+                            end
+                            return result
+                        end
