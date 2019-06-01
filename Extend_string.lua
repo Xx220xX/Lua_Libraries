bf55fdf4822b65b9fe234f33324962b66427e4ba
Henrique santos de Lima <hslhenriquegoscara@gmail.com>
2019-06-01 12:53:53 -0300

    fix bug

--- Extend_string.lua
+++ Extend_string.lua
@@ -1,17 +1,17 @@
 --[ transformar uma string em um array de char]--
-string.toArray = function (s)  
-                    local result ={} 
-                    for i=0,s:len(),1 do 
-                       result[i]=s:sub(i,i) 
-                    end 
-                    return result 
-                  end
---[ transformar um array de char em uma string]--
-string.arrayToString = function (array,fim) 
-                            local result =""    
-                            for i =1,#array,1 do
-                               if i>fim then break end
-                                result= result.. array[i] 
-                            end
-                            return result
-                        end
+string.toArrayInt = 
+function (s)  
+  local result ={} 
+  for i=0,s:len(),1 do
+    result[i]=s:byte(i) end return result 
+end
+--[ transformar um array de int em uma string]--
+string.arrayToString = 
+function (a,f) 
+    local result =""    
+    for i =1,#a,1 do
+       if i>f then break end
+          result= result.. string.char(a[i])
+        end
+     return result
+ end
