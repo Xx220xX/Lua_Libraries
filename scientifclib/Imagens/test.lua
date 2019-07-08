2e522bba71fb26bb2f8a4d857a13881e0034f520
Henrique santos de Lima <hslhenriquegoscara@gmail.com>
2019-07-07 21:19:55 -0300

    Update test.lua

--- scientifclib/Imagens/test.lua
+++ scientifclib/Imagens/test.lua
@@ -8,13 +8,17 @@
 
 Image = require('Image')
 Cor = require('Color')
-amarelo = Cor.new('RGB',255, 0 ,0)
-im = Image.new(10 ,10,amarelo,'RGB')
+Draw = require('Draw')
+amarelo = Cor.new('RGB', 255, 0, 0)
+im = Image.new(600, 600, Cor.white, 'RGB')
 --print(amarelo)
 --print(im)
-im:pixel(im.sh/2,im.sw/2,Cor.new('rgb',0,255))
 
-print(im:save("minhafototo","ppm"))
+Draw.circle(im,Cor.blue,im.sh/2,im.sw/2,(im.sh+im.sw)/20,(im.sh+im.sw)/600)
+--Draw.FUNCTION_X(im,Cor.cyan,(im.sh+im.sw)/600,math.sin,im.sw/2,im.sh/2,0.1,0.1)
+
+
+print(im:save("minhafototo", "ppm"))
 print(im.saved)
 
 
