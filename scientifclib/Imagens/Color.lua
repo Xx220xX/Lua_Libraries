9b8e634a755ccb0c18c206960ea94f81bb4c5b20
Henrique santos de Lima <hslhenriquegoscara@gmail.com>
2019-07-07 21:18:20 -0300

    adicionado cores pre definidas

--- scientifclib/Imagens/Color.lua
+++ scientifclib/Imagens/Color.lua
@@ -5,21 +5,28 @@
 -- Time: 10:55
 --
 local Color = {}
+
 Color.__index = Color
 Color.new = function(type, ...)
     local arg = { ... }
     local cor = {}
     if type:upper() == "RGB" then
         cor.type = type:upper()
-        cor.r, cor.g, cor.b = arg[1]or 0, arg[2] or 0, arg[3] or 0
+        cor.r, cor.g, cor.b = arg[1] or 0, arg[2] or 0, arg[3] or 0
         return setmetatable(cor, Color)
     end
 end
 Color.__tostring = function(v)
     local s = string.format("type[%s]", v.type)
     if v.type == "RGB" then
-        return s..string.format(" {r = %d, g = %d, b = %d}",v.r,v.g,v.b)
+        return s .. string.format(" {r = %d, g = %d, b = %d}", v.r, v.g, v.b)
     end
 end
+Color.blue = Color.new("RGB",0,0,255)
+Color.yellow = Color.new("RGB",255,255,0)
+Color.red = Color.new("RGB",255)
+Color.cyan = Color.new("RGB",0,255,255)
+Color.white = Color.new("RGB",255,255,255)
+
 return Color
 
