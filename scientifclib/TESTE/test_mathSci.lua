df96cd34f89033119802c91456ef14f7915d7a5c
Henrique santos de Lima <hslhenriquegoscara@gmail.com>
2019-07-09 16:29:17 -0300

    Create test_mathSci.lua

---
+++ scientifclib/TESTE/test_mathSci.lua
@@ -0,0 +1,25 @@
+sci = require('../MATH/mathSci')
+i = sci.complex.i
+
+function test(s,f,...)
+    print("test "..tostring(f), s.."   ".. f(...))
+end
+test("log",sci.log,i)           --
+test("log10",sci.log10,i)       --
+test("exp",sci.exp,i)           --
+test("srqt",sci.sqrt,-1)        --
+test("sin",sci.sin,i)           --
+test("cos",sci.cos,i)           --
+test("tan",sci.tan,i)           --
+test("asin",sci.asin,i)         --
+test("acos",sci.acos,i)         --
+test("atan",sci.atan,(i))       -- failed to z = i
+test("sinh",sci.sinh,(i))       --
+test("cosh",sci.cosh,(i))       --
+test("tanh",sci.tanh,(i))       --
+test("asinh",sci.asinh,(i))   -- failed to z = i
+test("acosh",sci.acosh,(i))     --
+test("atanh",sci.atanh,(i))     --
+
+
+
