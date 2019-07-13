fc3679b58889434ff8c1db14543af4390675ee8e
Henrique santos de Lima <hslhenriquegoscara@gmail.com>
2019-07-13 20:21:55 -0300

    finaly, i can plot a graph

--- scientifclib/TESTE/testImage.lua
+++ scientifclib/TESTE/testImage.lua
@@ -8,31 +8,25 @@ Image = require ('scientifclib.Imagens.Image')
 Cor = require('scientifclib.Imagens.Color')
 Draw = require('scientifclib.Imagens.Draw')
 amarelo = Cor.new('RGB', 255, 0, 0)
-im = Image.new(1080, 1080,25,25, Cor.white, 'RGB')
+im = Image.new(1000, 1000,25,25, Cor.white, 'RGB')
 --print(amarelo)
 --print(im)
 --Draw.circle(im,Cor.blue,im.sh/2,im.sw/2,(im.sh+im.sw)/20,(im.sh+im.sw)/600)
 
-local function f(x,y)
-    return 0.1*x^2
+local function f(x)
+    return 3*math.exp(-math.abs((x)^2))
 end
+Draw.axis(im,Cor.black,0.02,-5,5,-5,5)
 
-local function f1(x, y)
-    return y
-end
-
-local function f2(x,y)
-    return 30*x^3-x^2+0.00013 - y
-end
 Draw.plot(im,
---[[cor]] Cor.black,
---[[espessura ]] 2,
+--[[cor]] Cor.cyan,
+--[[espessura ]] 1,
 --[[f(x) =  y]] f,
---[[xi limite]] -10,
---[[xi limite]] 10,
---[[yi limite]] -10,
---[[yi limite]] 10)
+--[[xi limite]] -5,
+--[[xi limite]] 5,
+--[[yi limite]] -5,
+--[[yi limite]] 5)
+
 
 print(im:save("minhafototo", "ppm"))
-print(im.saved)
-
+os.execute('start '..im.saved)
